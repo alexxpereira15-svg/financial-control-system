74b7ba19575863f43d136cbe66374a8cf1c97b54
alexxpereira15-svg <alexxpereira15@gmail.com>
2026-08-02 03:05:51 -0600

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // <-- Se agrega '@tailwindcss/postcss' en lugar de 'tailwindcss'
     autoprefixer: {},
   },
 };
