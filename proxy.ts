129f73e5abd6ed87e7592c319890c788634b753e
alexxpereira15-svg <alexxpereira15@gmail.com>
2026-08-02 01:58:28 -0600

    Update and rename middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
@@ -35,7 +35,7 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  // Proteger rutas de (dashboard) si no hay sesión activa
+  // Proteger rutas si no hay sesión activa
   if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
