716c8cd7ed7911a2a830167eefdc7be41e365722
alexxpereira15-svg <alexxpereira15@gmail.com>
2026-08-17 16:50:05 -0600

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/usePathname' // O usePathname de 'next/navigation'
+import { usePathname } from 'next/navigation' // O usePathname de 'next/navigation'
 import {
   LayoutDashboard,
   Wallet,
